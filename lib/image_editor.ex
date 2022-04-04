b91c34ad9dd1c3d197b90cfcbbbac6b449a01fa8
cheropChirchir7963 <cheropmarlene@gmail.com>
2022-04-04 08:55:26 +0300

    initial code

---
+++ lib/image_editor.ex
@@ -0,0 +1,18 @@
+defmodule ImageEditor do
+  @moduledoc """
+  Documentation for `ImageEditor`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ImageEditor.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/image_editor_test.exs
@@ -0,0 +1,8 @@
+defmodule ImageEditorTest do
+  use ExUnit.Case
+  doctest ImageEditor
+
+  test "greets the world" do
+    assert ImageEditor.hello() == :world
+  end
+end
