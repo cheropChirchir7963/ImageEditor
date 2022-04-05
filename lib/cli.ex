21ef435a7da52ff35cd209826843bda235618758
cheropChirchir7963 <cheropmarlene@gmail.com>
2022-04-05 07:16:42 +0300

    refactured commands

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,39 +1,39 @@
 defmodule ImageEditor.Cli do
-  def parser(["I", column, row]) when is_integer(row) and is_integer(column) do
+  def parser([I, column, row]) when is_integer(row) and is_integer(column) do
     row
     |> ImageEditor.create_new_image(column)
     |> ImageEditor.Server.start_link()
   end
 
-  def parser(["S"]) do
+  def parser([S]) do
     ImageEditor.Server.show()
   end
 
-  def parser(["C"]) do
+  def parser([C]) do
     ImageEditor.Server.show()
     |> ImageEditor.reset_image_color()
     |> ImageEditor.Server.update_state()
   end
 
-  def parser(["L", column, row, color]) when is_integer(row) and is_integer(column) do
+  def parser([L, column, row, color]) when is_integer(row) and is_integer(column) do
     ImageEditor.Server.show()
     |> ImageEditor.apply_color(column, row, color)
     |> ImageEditor.Server.update_state()
   end
 
-  def parser(["H", start_column, end_column, row, color]) do
+  def parser([H, start_column, end_column, row, color]) do
     ImageEditor.Server.show()
     |> ImageEditor.drawing_horizontal_segment(start_column, end_column, row, color)
     |> ImageEditor.Server.update_state()
   end
 
-  def parser(["V", column, start_row, end_row, color]) do
+  def parser([V, column, start_row, end_row, color]) do
     ImageEditor.Server.show()
     |> ImageEditor.drawing_vertical_segment(column, start_row, end_row, color)
     |> ImageEditor.Server.update_state()
   end
 
-  def parser(["X"]) do
+  def parser([X]) do
     ImageEditor.Server.stop()
   end
 end
